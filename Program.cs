a14c62d6126ac6d7709f3d1f12afed3dbd0766fd
Natalia Khasanova <natell_2005@mail.ru>
2023-07-06 22:17:02 +0300

    Добавили решение задачи №4

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿Console.WriteLine("Введите первое число: ");
+﻿/*задача 2
+Console.WriteLine("Введите первое число: ");
 int a = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число: ");
 int b = Convert.ToInt32(Console.ReadLine());
@@ -9,4 +10,26 @@ if (a > b)
 else
 {
     Console.Write("максимальное число " + b);
+}
+
+*/
+//Задача 4
+Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+if (a > b && a > c)
+{
+    Console.Write("максимальнное число " + a);
+}
+if (b > a && b > c)
+{
+    Console.Write("максимальное число " + b);
+}
+if (c > b && c > a)
+{
+    Console.Write("максимальнное число " + c);
 }
